da7977b5a843f6b8a2a7e8ec3f07e7f9cc135aa6
Pedro1810 <vbanochke@mail.ru>
2019-11-27 11:27:11 +0300

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'show_code.dart';
 
-class TestHttp extends StatefulWidget {
-  final String url;
+class Article {
+  final int id;
+  final String name;
 
-  TestHttp({String url}):url = url;
+  Article({this.id, this.name});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      id: json['_id'],
+      name: json['name'],
+    );
+  }
+}
+
+class Articles {
+  final List<Article> articles;
+
+  Articles({this.articles});
+
+  factory Articles.fromJson(Map<String, dynamic> json) {
+    return Articles(
+      articles: json['articles'],
+    );
+  }
+}
+
+class TestHttp extends StatefulWidget {
+  final String url = 'https://all.culture.ru/api/2.3/articles';
 
   @override
   State<StatefulWidget> createState() => TestHttpState();
-}// TestHttp
+} // TestHttp
 
 class TestHttpState extends State<TestHttp> {
-  final _formKey = GlobalKey<FormState>();
 
   String _url, _body;
   int _status;
@@ -21,89 +45,47 @@ class TestHttpState extends State<TestHttp> {
   void initState() {
     _url = widget.url;
     super.initState();
-  }//initState
+  } //initState
 
   _sendRequestGet() {
-    if(_formKey.currentState.validate()) {
-      _formKey.currentState.save();//update form data
+    http.get(_url).then((response) {
+      _status = response.statusCode;
+      _body = response.body;
 
-      http.get(_url).then((response){
-        _status = response.statusCode;
-        _body = response.body;
+      final articles = Articles.fromJson(json.decode(response.body));
+      print(articles);
 
-        setState(() {});//reBuildWidget
-      }).catchError((error){
-        _status = 0;
-        _body = error.toString();
+      setState(() {}); //reBuildWidget
+    }).catchError((error) {
+      _status = 0;
+      _body = error.toString();
 
-        setState(() {});//reBuildWidget
-      });
-    }
-  }//_sendRequestGet
-
-  _sendRequestPost() async {
-    if(_formKey.currentState.validate()) {
-      _formKey.currentState.save();//update form data
-
-      try {
-        var response = await http.post(_url);
-
-        _status = response.statusCode;
-        _body = response.body;
-      } catch (error) {
-        _status = 0;
-        _body = error.toString();
-      }
-      setState(() {});//reBuildWidget
-    }
-  }//_sendRequestPost
-
-  _sendRequestPostBodyHeaders() async {
-    if(_formKey.currentState.validate()) {
-      _formKey.currentState.save();//update form data
-
-      try {
-        var response = await http.post(_url,
-                      body: {'name':'test','num':'10'},
-                      headers: {'Accept':'application/json'}
-                      );
-
-        _status = response.statusCode;
-        _body = response.body;
-      } catch (error) {
-        _status = 0;
-        _body = error.toString();
-      }
-      setState(() {});//reBuildWidget
-    }
-  }//_sendRequestPost
-
+      setState(() {}); //reBuildWidget
+    });
+  } //_sendRequestGet
 
   Widget build(BuildContext context) {
-    return Form(key: _formKey, child: SingleChildScrollView(child: Column(
-      children: <Widget>[
-        Container(
-            child: Text('API url', style: TextStyle(fontSize: 20.0,color: Colors.blue)),
-            padding: EdgeInsets.all(10.0)
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            RaisedButton(
+                child: Text('Send request GET'), onPressed: _sendRequestGet),
+            SizedBox(height: 20.0),
+            Text('Response status',
+                style: TextStyle(fontSize: 20.0, color: Colors.blue)),
+            Text(_status == null ? '' : _status.toString()),
+            SizedBox(height: 20.0),
+            Text('Response body',
+                style: TextStyle(fontSize: 20.0, color: Colors.blue)),
+            Text(_body == null ? '' : _body),
+          ],
         ),
-        Container(
-            child: TextFormField(initialValue: _url, validator: (value){if (value.isEmpty) return 'API url isEmpty';}, onSaved: (value){_url = value;}, autovalidate: true),
-            padding: EdgeInsets.all(10.0)
-        ),
-        SizedBox(height: 20.0),
-        RaisedButton(child: Text('Send request GET'), onPressed: _sendRequestGet),
-        RaisedButton(child: Text('Send request POST'), onPressed: _sendRequestPost),
-        RaisedButton(child: Text('Send request POST with Body and Headers'), onPressed: _sendRequestPostBodyHeaders),
-        SizedBox(height: 20.0),
-        Text('Response status', style: TextStyle(fontSize: 20.0,color: Colors.blue)),
-        Text(_status == null ? '' :_status.toString()),
-        SizedBox(height: 20.0),
-        Text('Response body', style: TextStyle(fontSize: 20.0,color: Colors.blue)),
-        Text(_body == null ? '' : _body),
-      ],
-    )));
-  }//build
-}//TestHttpState
+      ),
+    );
+  } //build
+} //TestHttpState
 
 class MyApp extends StatelessWidget {
   @override
@@ -111,18 +93,19 @@ class MyApp extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('Test HTTP API'),
-          actions: <Widget>[IconButton(icon: Icon(Icons.code), tooltip: 'Code', onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => CodeScreen()));
-          })],
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.code),
+                tooltip: 'Code',
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CodeScreen()));
+                })
+          ],
         ),
-        body: TestHttp(url: 'https://json.flutter.su/echo')
-    );
+        body: TestHttp());
   }
 }
 
-void main() => runApp(
-    MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyApp()
-    )
-);
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
