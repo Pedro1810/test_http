386992f8487779e3c008bd12d03890e3f4175a36
Pedro1810 <vbanochke@mail.ru>
2019-12-24 22:08:41 +0300

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'show_code.dart';
+import 'dart:convert';
 
-class Article {
-  final int id;
-  final String name;
+class PictureInfo{
+  final double height;
+  final double width;
+  final String breeds;
+  final String url;
 
-  Article({this.id, this.name});
+  PictureInfo({this.height, this.width, this.breeds, this.url});
 
-  factory Article.fromJson(Map<String, dynamic> json) {
-    return Article(
-      id: json['_id'],
-      name: json['name'],
-    );
-  }
-}
+  factory PictureInfo.fromJson(Map<String, dynamic> json) {
+    var streetsFromJson = json['streets'];
+    List<String> streetsList = new List<String>.from(streetsFromJson);
 
-class Articles {
-  final List<Article> articles;
-
-  Articles({this.articles});
-
-  factory Articles.fromJson(Map<String, dynamic> json) {
-    return Articles(
-      articles: json['articles'],
+    return PictureInfo(
+      breeds: json['breeds'],
+      height: json['height'],
+      width: json['width'],
+      url: json['url'],
     );
   }
 }
 
 class TestHttp extends StatefulWidget {
-  final String url = 'https://all.culture.ru/api/2.3/articles';
+  final String url = "https://api.thecatapi.com/v1/images/search";
 
   @override
   State<StatefulWidget> createState() => TestHttpState();
-} // TestHttp
+}// TestHttp
 
 class TestHttpState extends State<TestHttp> {
-
-  String _url, _body;
+  String _url;
+  PictureInfo _pictureInfo = PictureInfo(
+      breeds: '',
+      height: 0,
+      width: 0,
+      url: ''
+  );
   int _status;
 
   @override
   void initState() {
     _url = widget.url;
     super.initState();
-  } //initState
+  }//initState
 
   _sendRequestGet() {
-    http.get(_url).then((response) {
+
+    http.get(_url).then((response){
       _status = response.statusCode;
-      _body = response.body;
-
-      final articles = Articles.fromJson(json.decode(response.body));
-      print(articles);
-
-      setState(() {}); //reBuildWidget
-    }).catchError((error) {
+      _pictureInfo = PictureInfo.fromJson(json.decode(response.body));
+      setState(() {});//reBuildWidget
+    }).catchError((error){
       _status = 0;
-      _body = error.toString();
-
-      setState(() {}); //reBuildWidget
+      print(error);
+      print("Response status: $_status");
+      setState(() {});//reBuildWidget
     });
-  } //_sendRequestGet
+  }//_sendRequestGet
 
   Widget build(BuildContext context) {
-    return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 20.0),
-            RaisedButton(
-                child: Text('Send request GET'), onPressed: _sendRequestGet),
-            SizedBox(height: 20.0),
-            Text('Response status',
-                style: TextStyle(fontSize: 20.0, color: Colors.blue)),
-            Text(_status == null ? '' : _status.toString()),
-            SizedBox(height: 20.0),
-            Text('Response body',
-                style: TextStyle(fontSize: 20.0, color: Colors.blue)),
-            Text(_body == null ? '' : _body),
-          ],
-        ),
-      ),
-    );
-  } //build
-} //TestHttpState
+    return Center(child: SingleChildScrollView(child: Column(
+      children: <Widget>[
+        SizedBox(
+            height: 20.0),
+        RaisedButton(
+            child: Text('Send request GET'),
+            onPressed: _sendRequestGet),
+
+        SizedBox(height: 20.0),
+        Text(_pictureInfo.breeds),
+
+        SizedBox(height: _pictureInfo.height, width: _pictureInfo.width),
+        Image.network(_pictureInfo.url),
+      ],
+    )));
+  }//build
+}//TestHttpState
 
 class MyApp extends StatelessWidget {
   @override
@@ -93,19 +86,18 @@ class MyApp extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('Test HTTP API'),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.code),
-                tooltip: 'Code',
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => CodeScreen()));
-                })
-          ],
+          actions: <Widget>[IconButton(icon: Icon(Icons.code), tooltip: 'Code', onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => CodeScreen()));
+          })],
         ),
-        body: TestHttp());
+        body: TestHttp()
+    );
   }
 }
 
-void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+void main() => runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyApp()
+    )
+);
